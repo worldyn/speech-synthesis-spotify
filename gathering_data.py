7e25e132a9a15de51d83fe0f8b1fd2eb1f29dca2
Javier García San Vicente <javigarciasv@gmail.com>
2021-02-17 20:17:10 +0100

    Parallel data gathering

--- gathering_data.py
+++ gathering_data.py
@@ -1,23 +1,129 @@
 import os
+import time
+import logging
+import threading
+import time
 
-LOCAL_DATA_ROOT = './data/spotify-podcasts-2020/podcasts-transcripts/'
+
+LOCAL_DATA_ROOT = './data/spotify-podcasts-2020/podcasts-transcripts'
 
 REMOTE_AUDIO_ROOT = './Spotify-Podcasts-2020/podcasts-audio-only-2TB/podcasts-audio'
 
 REMOTE_TRANSCRIPT_ROOT = './Spotify-Podcasts-2020/podcasts-no-audio-13GB'
 
+def log_print(filename, text):
+    with open(filename, 'a') as file:
+        file.write(text)
 
-for subdir, dirs, files in os.walk(LOCAL_DATA_ROOT):
-    for filename in files:
-        podcast_id = filename.split('.')[0]
-        remote_audio_path = subdir.replace(LOCAL_DATA_ROOT, REMOTE_AUDIO_ROOT) + '/' + podcast_id + '.ogg'
-        remote_audio_path = remote_audio_path.replace('\\','/')
-        remote_audio_path = remote_audio_path[1:]
+def collect_files():
 
-        local_transcript_path = subdir.replace('\\','/')
+    podcasts_ids = []
 
-        os.system('get-data.sh' + ' ' + remote_audio_path + ' ' + local_transcript_path)
+    for subdir, dirs, files in os.walk(LOCAL_DATA_ROOT):
+        for filename in files:
 
-        local_ogg_audio_file = local_transcript_path + '/' + podcast_id + '.ogg'
-        local_wav_audio_file = local_transcript_path + '/' + podcast_id + '.wav'
-        os.system('ffmpeg' + ' ' + '-i'  + ' ' + local_ogg_audio_file + ' ' + local_wav_audio_file)
+            podcast_id = filename.split('.')[0]
+
+            podcasts_ids.append((subdir, podcast_id))
+
+    return podcasts_ids[:4]
+
+
+def retrieve_batch(batch):   
+
+    log_file = 'retrieve.log'
+
+    log_print(log_file, '\n\n### NEW BATCH ###\n')   
+
+    args = ''
+
+    for subdir, podcast_id in batch:
+
+        audio_file = subdir.replace(LOCAL_DATA_ROOT, '') + '/' + podcast_id + '.ogg'
+        audio_file = audio_file.replace('\\','/')
+        audio_file = audio_file[1:]
+
+        args += ' ' + audio_file
+
+    log_print(log_file, 'Downloading ' + str(len(batch)) + ' audio files...')
+    start = time.time()
+    os.system('get-data.sh' + args)
+    end = time.time()
+    log_print(log_file, 'Download of ' + str(len(batch)) + ' files completed in ' + str(end - start))
+    log_print(log_file, '\nFiles processed: \n' + str(batch))
+
+
+
+def convert_batch(batch):
+
+    log_file = 'convert.log'   
+
+    log_print(log_file, '\n\n### NEW BATCH ###\n')  
+
+    log_print(log_file, 'Converting ' + str(len(batch)) + ' audio files...')
+    start = time.time()
+    for subdir, podcast_id in batch:
+
+        transcript_path = subdir.replace('\\','/')
+        
+        ogg_file = transcript_path + '/' + podcast_id + '.ogg'
+        wav_file = transcript_path + '/' + podcast_id + '.wav'
+
+        os.system('ffmpeg' + ' ' + '-n'  + ' ' + '-i'  + ' ' + ogg_file + ' ' + wav_file)
+
+    end = time.time()
+    log_print(log_file, 'Conversion of ' + str(len(batch)) + ' files completed in ' + str(end - start))
+    log_print(log_file, '\nFiles processed: \n' + str(batch))
+
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+
+files = collect_files()
+
+
+workload = len(files)
+batch_size = 4
+
+divisions = list(range(0, workload, batch_size))
+divisions.append(len(files))
+
+batches = []
+
+for i in range(1,len(divisions)):
+    batch = files[divisions[i-1] : divisions[i]]
+    batches.append(batch)
+
+# Retrieve the first batch without paralelism
+retrieve_batch(batches[0])
+# Retrieve the batch[i] while converting the batch[i-1]
+for batch_index in range(1,len(batches)):
+
+    convert_thread = threading.Thread(target=convert_batch, args=(batches[batch_index-1],))
+    convert_thread.start()
+
+    retrieve_batch(batches[batch_index])
+
+    convert_thread.join()
+
+# Convert the last batch without paralelism
+convert_batch(batches[-1])
+
+exit()
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
--- get-data.sh
+++ get-data.sh
@@ -5,5 +5,28 @@
 # Running: exec get-data.sh {spotify-dir} 
 # Example: exec get-data.sh Spotify-Podcasts-2020/podcasts-audio-only-2TB/podcasts-audio/0/F/show_0Fvt7eaJFKwwVengB9x9IZ/7zIPUGJIClsFZUTgB4BuQ2.ogg
 
-rclone copy -P trecbox:$1 $2
+# For each complete path
+for var in "$@"
+do
+    # Split the path according to /
+    IFS=$'/'
+    read -a remotepatharr <<< "$var"
 
+    # Removing the last component of the array
+    localpathstr=${remotepatharr[@]::${#remotepatharr[@]}-1}
+
+    # Split the resultant text to join it with /
+    IFS=$' '
+    read -a localpatharr <<< "$localpathstr"
+    IFS=$'/'
+    localpathstr="${localpatharr[*]}"
+    IFS=$' '
+
+    remotepath="trecbox:Spotify-Podcasts-2020/podcasts-audio-only-2TB/podcasts-audio/$var"
+    localpath="./data/spotify-podcasts-2020/podcasts-transcripts/$localpathstr"
+
+    printf "\nDownloading file: $remotepath \nInto: $localpath\n\n"
+
+    # Retrieve the remote audio file into the local path
+    rclone copy -P $remotepath $localpath
+done
