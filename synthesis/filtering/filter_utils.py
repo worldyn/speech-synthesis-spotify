0cdbbeba4898968b167382e2bc118a316887ff72
Adam J <adamjac@kth.se>
2021-02-25 15:54:13 +0100

    created simple filter utils

--- synthesis/filtering/filter_utils.py
+++ synthesis/filtering/filter_utils.py
@@ -9,13 +9,19 @@ import matplotlib.pyplot as plt
 
 path = './testdata/smaller7tmono.wav'
 
-fs_wav, data_wav = wavfile.read(path) # sampling rate and audio data
+def get_wav(path):
+    _,w = wavfile.read(path)
+    return w
+
+def trim(wavdata):
+
+data_wav = get_wav(path) # sampling rate and audio data
 
 # normalize, assumes 16 bits per sample
 data_wav_norm = data_wav / (2**15)
 
 time_wav = np.arange(0, len(data_wav)) / fs_wav
-plt.plot(data_wav)
+plt.plot(data_wav_norm)
 plt.show()
 
 
