879a1e60bf959367f9a8167a398777c669fd3876
Adam J <adamjac@kth.se>
2021-02-26 15:50:49 +0100

    pitch,intensity,spectrogram calculation

--- synthesis/filtering/filter_utils.py
+++ synthesis/filtering/filter_utils.py
@@ -7,7 +7,10 @@ import plotly
 import numpy as np
 import matplotlib.pyplot as plt
 import IPython
+import parselmouth
 
+
+# Great help from: https://hackernoon.com/audio-handling-basics-how-to-process-audio-files-using-python-cli-jo283u3y
 
 # retuns sample rate and wav data
 def get_wav(path):
@@ -42,19 +45,80 @@ def remove_silent_segments(segments, energy_thres=0.5):
 def write_wav(path, signal, fs):
     wavfile.write(path, fs, signal)
 
+def plot_sound(path):
+    snd = parselmouth.Sound("./testdata/smaller7tmono.wav")
+    plt.figure()
+    plt.plot(snd.xs(), snd.values.T)
+    plt.xlim([snd.xmin, snd.xmax])
+    plt.xlabel("time [s]")
+    plt.ylabel("amplitude")
+    plt.show()
+
+class Sound:
+    def __init__(self, path):
+        self.data = parselmouth.Sound(path)
+        self.spectrogram = self.data.to_spectrogram()
+        self.intensity = self.data.to_intensity()
+        self.pitch_obj = self.data.to_pitch()
+        self.pitch = self.pitch_obj.selected_array['frequency']
+        # pre-emphasize
+        self.data_emp = self.data.copy().pre_emphasize()
+
+    def draw_spectrogram(self, dynamic_range=70):
+        X, Y = self.spectrogram.x_grid(), self.spectrogram.y_grid()
+        sg_db = 10 * np.log10(self.spectrogram.values)
+        plt.pcolormesh(
+            X,Y,sg_db,vmin=sg_db.max() - dynamic_range,cmap='afmhot'
+        )
+        plt.ylim([self.spectrogram.ymin, self.spectrogram.ymax])
+        plt.xlabel("time [s]")
+        plt.ylabel("frequency [Hz]")
+
+    def draw_intensity(self):
+        plt.plot(
+            self.intensity.xs(), self.intensity.values.T, 
+            linewidth=3, color='g'
+        )
+        plt.plot(self.intensity.xs(), self.intensity.values.T, linewidth=1)
+        plt.grid(False)
+        plt.ylim(0)
+        plt.ylabel("intensity [dB]")
+        
+    def draw_pitch(self):
+        pitch_values = self.pitch
+        pitch_values[pitch_values == 0] = np.nan
+        plt.plot(
+            self.pitch_obj.xs(), pitch_values, 'o',
+            markersize=5, color='w'
+        )
+        plt.plot(self.pitch_obj.xs(), pitch_values, 'o', markersize=2)
+        plt.grid(False)
+        plt.ylim(0, self.pitch_obj.ceiling)
+        plt.ylabel("fundamental frequency [Hz]")
+
 def main():
+    # TODO: 
+    # silence removal (done)
+    # trim (done)
+    # speech rate (spotify paper)
+    # music?
     path = './testdata/smaller7tmono.wav'
     fs_wav, data_wav = get_wav(path) # sampling rate and audio data
 
     # normalize, assumes 16 bits per sample
-    data_wav_norm = data_wav / (2**15)
+    #data_wav_norm = data_wav / (2**15)
 
-    segments = segment_interval(data_wav_norm, fs_wav, 1)
-    data_no_silence = remove_silent_segments(segments)
+    #segments = segment_interval(data_wav_norm, fs_wav, 1)
+    #data_no_silence = remove_silent_segments(segments)
 
-    write_wav("./testdata/nosilence.wav",data_no_silence,fs_wav)
+    #write_wav("./testdata/nosilence.wav",data_no_silence,fs_wav)
 
-    plt.plot(data_wav_norm)
+    #plt.plot(data_wav_norm)
+    snd = Sound("./testdata/smaller7tmono.wav")
+    #snd.draw_intensity()
+    snd.draw_spectrogram()
+    snd.draw_pitch()
+
     plt.show()
 
 main()
