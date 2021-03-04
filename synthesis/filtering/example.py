d218fbca94f4678a989d618d7b16b8661d31f41e
Adam J <adamjac@kth.se>
2021-03-04 10:39:12 +0100

    testing some filtering stuff

--- synthesis/filtering/example.py
+++ synthesis/filtering/example.py
@@ -2,20 +2,25 @@ from utilsfilter import *
 from segment import Segment
 
 def main():
-    testpath = './smaller7tmono.wav'
+    testpath = './testdata/with-noise.wav'
     #testpath = './testdata/7quCH1LZWMKX9xg2tJBMsP.wav'
     fs_wav, data_wav = get_wav(testpath)
-    print("fs wav: ", fs_wav)
-    #samp_freq = 
-    fs = 44100.0
-    # start end time not used on wav file
+    fs = 44100
+    data_wav_norm = data_wav / (2**15) # normalize to -1,1
+    data_intervals = segment_interval(
+        data_wav_norm, fs, 2.5
+    )
+    data_silence = remove_silent_segments(data_intervals) #* 2**15
+    write_wav("./testdata/tyst.wav",data_silence,fs)
+    
+    ##
     test_text = "The media and of story is a business. now when it comes to business everybody has a reason a very personal one for why they're in that particular business and everyone has their own personal best interests to consider."
-    seg = Segment(testpath, 0, 20, fs, test_text)
+    #seg = Segment(testpath, 0, 20, fs, test_text)
     #print("Amplitude")
     #seg.draw_data()
     #seg.write("test.wav")
     #plt.show()
-    sr = seg.get_speech_rate()
-    print("SPEECH RATE:" , sr)
+    #sr = seg.get_speech_rate()
+    #print("SPEECH RATE:" , sr)
 main()
 
