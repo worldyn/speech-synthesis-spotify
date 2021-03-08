b5c6d8b1441dd529bebedc9a7bd57bc0e571fbd5
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-03-08 19:11:18 +0100

    feature: glue together nearby short segments

--- synthesis/breath/segment.py
+++ synthesis/breath/segment.py
@@ -4,6 +4,10 @@ import numpy as np
 from tqdm import tqdm
 
 
+target_segment_seconds = 10
+max_empty_seconds = 1
+
+
 def process_episode(annotation_path: Path):
     with np.load(annotation_path) as annotation_file:
         predictions = annotation_file["arr_0"]
@@ -22,14 +26,23 @@ def process_episode(annotation_path: Path):
         if prev_prediction in allowed_classes and next_prediction not in allowed_classes
     ]
     segments = list(zip(starts, ends))
+    segments_seconds = (np.array(segments) * 0.05).tolist()
+
+    joined_segments = []
+    for segment in segments_seconds:
+        if len(joined_segments) == 0:
+            joined_segments.append(segment)
+            continue
+        if (
+            segment[1] - joined_segments[-1][0] < target_segment_seconds
+            and segment[0] - joined_segments[-1][1] < max_empty_seconds
+        ):
+            joined_segments[-1][1] = segment[1]
+        else:
+            joined_segments.append(segment)
 
     with open(annotation_path.name.replace("npz", "json"), "w") as file:
-        json.dump(indices_to_seconds(segments), file)
-
-
-def indices_to_seconds(arr):
-    arr = np.array(arr) * 0.05
-    return arr.tolist()
+        json.dump(joined_segments, file)
 
 
 if __name__ == "__main__":
