9f734e87dbf533e029bb599f461ec604bdc9c8f6
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-03-15 11:42:17 +0100

    improve: use vowel groups to approximate speech rate

--- synthesis/filter/segment.py
+++ synthesis/filter/segment.py
@@ -1,47 +1,37 @@
+import re
 from functools import lru_cache
 from cached_property import cached_property
 import numpy as np
 from scipy.io import wavfile
 import parselmouth
-from g2p_en import G2p
 
 
 class Segment:
-    def __init__(self, path, start_time, end_time, samp_freq=44100, text=""):
+    def __init__(self, path, start_time, end_time, text, sample_rate=44100):
         self.path = path
         self.start_time = start_time
         self.end_time = end_time
         self.duration = end_time - start_time
 
-        sound = read_sound_file(path).extract_part(
+        sound: parselmouth.Sound = read_sound_file(path).extract_part(
             from_time=start_time, to_time=end_time
         )
         self.amplitudes = sound.values.T
 
-        self.samp_freq = samp_freq
-        self.intensities = sound.to_intensity()
-        self.pitches = sound.to_pitch().selected_array["frequency"]
+        self.sample_rate = sample_rate
+        self.intensity = np.mean(sound.to_intensity(subtract_mean=False))
+        self.pitch = np.mean(sound.to_pitch().selected_array["frequency"])
         self.energy = sound.get_energy()
 
         self.text = text
 
     def write(self, path):
-        wavfile.write(path, self.samp_freq, self.amplitudes)
-
-    @cached_property
-    def pitch(self):
-        return np.mean(self.pitches)
-
-    @cached_property
-    def intensity(self):
-        return np.mean(self.intensities)
+        wavfile.write(path, self.sample_rate, self.amplitudes)
 
     @cached_property
     def speech_rate(self):
-        g2p = G2p()
-        phonemes = g2p(self.text)
-        phonemes = [s for s in phonemes if s.strip()]
-        return len(phonemes) / self.duration
+        vowel_groups = re.findall("[aeiou]+", self.text)
+        return len(vowel_groups) / self.duration
 
     def __eq__(self, other):
         return hash(self) == hash(other)
@@ -51,5 +41,5 @@ class Segment:
 
 
 @lru_cache(maxsize=1)
-def read_sound_file(path):
+def read_sound_file(path) -> parselmouth.Sound:
     return parselmouth.Sound(path)
